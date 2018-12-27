a7bdf9fbc568288819cb06bfea90bfc2aa64b498
Gaspard Ferey <gaspard.ferey@inria.fr>
2018-12-27 15:15:31 +0100

    WIP

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
+GITPATH=/home/pi/git/raspberry
+WWWPATH=/var/www
+
 # Fetching latest website
-cd /home/pi/git/raspberry/
+cd $GITFOLDER
 git pull
 
-# Move /home/pi/git/raspberry/ folders to /var/www/ but keeping /var/www/backend/instance
-mv /var/www/backend/instance /var/www/instance
-rm -rf /var/www/coloc
-rm -rf /var/www/backend
-cp -r /home/pi/git/raspberry/coloc   /var/www/coloc
-cp -r /home/pi/git/raspberry/backend /var/www/backend
-mv /var/www/instance /var/www/backend/instance
+# Move $GITPATH// folders to $WWWPATH/ but keeping $WWWPATH/backend/instance
+mv $WWWPATH/backend/instance $WWWPATH/instance
+rm -rf $WWWPATH/coloc
+rm -rf $WWWPATH/backend
+cp -r $GITPATH/coloc   $WWWPATH/coloc
+cp -r $GITPATH/backend $WWWPATH/backend
+mv $WWWPATH/instance $WWWPATH/backend/instance
 
 # Getting (latest) Bootstrap files
-wget -O /var/www/backend/backend/static/bootstrap.css 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css'  
-wget -O /var/www/backend/backend/static/jquery.js     'https://code.jquery.com/jquery-3.3.1.slim.min.js'
-wget -O /var/www/backend/backend/static/popper.js     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js'
-wget -O /var/www/backend/backend/static/bootstrap.js  'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js'
+wget -nv -O $WWWPATH/backend/backend/static/bootstrap.css 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css'  
+wget -nv -O $WWWPATH/backend/backend/static/jquery.js     'https://code.jquery.com/jquery-3.3.1.slim.min.js'
+wget -nv -O $WWWPATH/backend/backend/static/popper.js     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js'
+wget -nv -O $WWWPATH/backend/backend/static/bootstrap.js  'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js'
 
 # Fixing rights
-sudo chown -R www-data:www-data /var/www/
-sudo chmod -R 775 /var/www/
-sudo chmod 664 /var/www/backend/instance/*
+sudo chown -R www-data:www-data $WWWPATH/
+sudo chmod -R 775 $WWWPATH/
+sudo chmod 664 $WWWPATH/backend/instance/*
 
 # Restarting apache service
 sudo /etc/init.d/apache2 restart
 
 
-export FLASK_APP=/var/www/backend/backend
+export FLASK_APP=$WWWPATH/backend/backend
 export FLASK_ENV=development
 
 flask init-db
