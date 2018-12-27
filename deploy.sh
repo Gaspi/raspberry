782de08fdd41aa16ea2848ebc5573cc09ff045af
Gaspard Férey <gaspard.ferey@hotmail.fr>
2018-12-27 10:14:55 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,12 @@ cp -r /home/pi/git/raspberry/coloc   /var/www/coloc
 cp -r /home/pi/git/raspberry/backend /var/www/backend
 mv /var/www/instance /var/www/backend/instance
 
+# Getting (latest) Bootstrap files
+wget https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css  /var/www/backend/backend/static/bootstrap.css
+wget https://code.jquery.com/jquery-3.3.1.slim.min.js                          /var/www/backend/backend/static/jquery.js
+wget https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js /var/www/backend/backend/static/popper.js
+wget https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js    /var/www/backend/backend/static/bootstrap.js
+
 # Fixing rights
 sudo chown -R www-data:www-data /var/www/
 sudo chmod -R 775 /var/www/
