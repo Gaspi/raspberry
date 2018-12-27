fd91181d4c587eeda32842490ccc2c169dcaa441
Gaspard Ferey <gaspard.ferey@inria.fr>
2018-12-27 12:58:52 +0100

    WIP

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,10 @@ cp -r /home/pi/git/raspberry/backend /var/www/backend
 mv /var/www/instance /var/www/backend/instance
 
 # Getting (latest) Bootstrap files
-wget https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css  /var/www/backend/backend/static/bootstrap.css
-wget https://code.jquery.com/jquery-3.3.1.slim.min.js                          /var/www/backend/backend/static/jquery.js
-wget https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js /var/www/backend/backend/static/popper.js
-wget https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js    /var/www/backend/backend/static/bootstrap.js
+wget -O /var/www/backend/backend/static/bootstrap.css 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css'  
+wget -O /var/www/backend/backend/static/jquery.js     'https://code.jquery.com/jquery-3.3.1.slim.min.js'
+wget -O /var/www/backend/backend/static/popper.js     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js'
+wget -O /var/www/backend/backend/static/bootstrap.js  'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js'
 
 # Fixing rights
 sudo chown -R www-data:www-data /var/www/
